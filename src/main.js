4740e410f6d5883f9d84188f563e9b5f2abe0b06
Mohaymen Sameen <42969169+MohaymenSameen@users.noreply.github.com>
2023-05-28 23:36:11 +0200

    Fix urls

--- src/main.js
+++ src/main.js
@@ -16,8 +16,8 @@ const houses = [];
 const { CHAT_ID, BOT_API } = process.env;
 
 const urls = [
-    'https://www.funda.nl/en/koop/amsterdam/beschikbaar/0-300000/40+woonopp/2+slaapkamers/1-dag/',
-    'https://www.funda.nl/en/koop/haarlem/beschikbaar/0-300000/40+woonopp/2+slaapkamers/1-dag/',
+    'https://www.funda.nl/en/huur/utrecht/0-1250/+15km/sorteer-datum-af/',
+    'https://www.funda.nl/en/huur/nieuwegein/0-1250/1-dag/+5km/sorteer-datum-af/',
 ];
 
 const runTask = async () => {
